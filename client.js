b710835fbefadc8529cf82d219888b783177fe4a
oluseunakin <akindoyinoluseun@gmail.com>
2023-02-03 14:07:06 +0100

    redesigned

--- client.js
+++ client.js
@@ -1,40 +1,36 @@
 const baseURL = "https://webserver.herokuapp.com"
-
+//const baseURL = "http://localhost:3000";
 function init() {
-    const main = document.getElementById('main')
-    
-    main.innerHTML = '<p>Loading</p>'
-    fetch(`${baseURL}/getprojects`).then(async (response) => {
-        const projectArray = await response.json()
-        const projectsUL = document.getElementById('projects')
-        projectArray.forEach(async (proj, i) => {
-            const li = document.createElement('li')
-            const path = proj.replace(".txt", "")
-            const link = document.createElement('a')
-            link.href = `${baseURL}/project/${path}`
-            link.innerText = path
-            li.appendChild(link)
-            projectsUL.appendChild(li)
-            if(i === 0) {
-                const title = document.createElement('h1')
-                const description = document.createElement('p')
-                const stack = document.createElement('h3')
-                let data = await (await fetch(`${baseURL}/data/${proj}`)).json()
-                data = JSON.parse(data)
-                main.innerHTML = ''
-                title.innerText = data.name[0]
-                description.innerText = data.description[0]
-                data.tech.forEach(s => {
-                    const span = document.createElement('span')
-                    span.innerText = s
-                    stack.appendChild(span)
-                })
-                main.appendChild(title)
-                main.appendChild(stack)
-                main.appendChild(description)
-            }
-        })
-    }).catch(e => {
-        console.log(e)
+  const main = document.getElementById("main");
+  main.innerHTML = "<p>Loading</p>";
+  fetch(`${baseURL}/getprojects`)
+    .then(async (response) => {
+      const projects = await response.json();
+      const projectsUL = document.getElementById("projects");
+      Object.keys(projects).forEach((p, i) => {
+        const proj = projects[p];
+        const name = proj.name;
+        const li = document.createElement("li");
+        const link = document.createElement("a");
+        link.href = `${baseURL}/projects/${p}`;
+        link.innerText = name;
+        li.appendChild(link);
+        projectsUL.appendChild(li);
+        if (i === 0) {
+          const title = document.createElement("h1");
+          const description = document.createElement("p");
+          const stack = document.createElement("h3");
+          main.innerHTML = "";
+          title.innerText = name;
+          description.innerText = proj.description;
+          stack.innerText = proj.tech
+          main.appendChild(title);
+          main.appendChild(stack);
+          main.appendChild(description);
+        }
+      });
     })
+    .catch((e) => {
+      console.log(e);
+    });
 }
